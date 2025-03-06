177f3a09bc3e0ee633905744caeaf476658344d7
Nastyaskkdask <144699427+Nastyaskkdask@users.noreply.github.com>
2025-03-07 00:19:29 +0500

    n1.fs

--- n1.fs
+++ n1.fs
@@ -1,5 +1,32 @@
 open System
 
+printf "Введите кол-во строк: "
+let size = Console.ReadLine()
+let check1 = 
+    if size = "0" then
+        printfn "Список пуст."
+        exit 1 
+    match System.Int32.TryParse(size) with
+    | true, parsedInt -> parsedInt
+    | false, _ -> 
+        printfn "Ошибка: Введите целое число."
+        exit 1 
+
+let minL = 3
+let maxL = 7
+
+printf "Введите символ для добавления в конец: "
+let symbol = Console.ReadLine()
+let check2 =
+    if String.IsNullOrEmpty(symbol) then
+        printfn "Ошибка: Вы не ввели символ."
+        exit 1
+    elif symbol.Length > 1 then
+        printfn "Ошибка: Введите только один символ."
+        exit 1
+    else
+        symbol.[0]
+
 let generateRand (count: int) (minL: int) (maxL: int) =
     let rnd = new Random()
     [ for i in 1..count do
@@ -12,35 +39,6 @@ let generateRand (count: int) (minL: int) (maxL: int) =
 
 let addToList (add: char) (sList: string list) =
     List.map (fun str -> str + string add) sList
-
-printf "Введите кол-во строк: "
-let size = Console.ReadLine()
-
-let check1 = 
-    if size = "0" then
-        printfn "Список пуст."
-        exit 1 
-    match System.Int32.TryParse(size) with
-    | true, parsedInt -> parsedInt
-    | false, _ -> 
-        printfn "Ошибка: Введите целое число."
-        exit 1 
-    
-let minL = 3
-let maxL = 7
-
-printf "Введите символ для добавления в конец: "
-let symbol = Console.ReadLine()
-
-let check2 =
-    if String.IsNullOrEmpty(symbol) then
-        printfn "Ошибка: Вы не ввели символ."
-        exit 1
-    elif symbol.Length > 1 then
-        printfn "Ошибка: Введите только один символ."
-        exit 1
-    else
-        symbol.[0]
 
 let MyList = generateRand check1 minL maxL
 printfn "Исходный список:"
