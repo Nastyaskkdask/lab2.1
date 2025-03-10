4ae6a3b1220257c6dc24a69f3eee01152c264cd8
Nastyaskkdask <144699427+Nastyaskkdask@users.noreply.github.com>
2025-03-10 22:11:41 +0500

    n1.fs

--- n1.fs
+++ n1.fs
@@ -1,22 +1,67 @@
 open System
 
-printf "Введите кол-во строк: "
-let size = Console.ReadLine()
-let check1 = 
-    if int(size) <= 0 then
-        printfn "Список пуст."
-        exit 1 
-    match System.Int32.TryParse(size) with
-    | true, parsedInt -> parsedInt
-    | false, _ -> 
-        printfn "Ошибка: Введите целое число."
-        exit 1 
+let rnd = new Random()
 
-let minL = 3
-let maxL = 7
+let generateRandomString (minL: int) (maxL: int) =
+    let length = rnd.Next(minL, maxL + 1)
+    let chars = Array.zeroCreate length
+    for i in 0..length - 1 do
+        let charType = rnd.Next(3) 
+        chars[i] <-
+            match charType with
+            | 0 -> char (rnd.Next(10) + int '0') 
+            | 1 -> char (rnd.Next(26) + int 'a') 
+            | 2 -> char (rnd.Next(26) + int 'A') 
+            | _ -> ' ' 
+    String chars
+
+let generateRand (count: int) (minL: int) (maxL: int) =
+    [ for i in 1..count do
+        generateRandomString minL maxL ]
+
+let addToList (add: char) (sList: string list) =
+    List.map (fun str -> str + string add) sList
+
+
+printf "Выберите способ ввода списка строк (1 - рандомный, 2 - ручной): "
+let inputMode = Console.ReadLine()
+
+let MyList =
+    match inputMode with
+    | "1" ->
+        printf "Введите кол-во строк: "
+        let size = Console.ReadLine()
+        if size = "0" then
+            printfn "\nСписок пуст."
+            exit 1
+        match System.Int32.TryParse(size) with
+        | true, parsedInt ->
+            let minL = 3
+            let maxL = 7
+            generateRand parsedInt minL maxL
+        | false, _ ->
+            printfn "Ошибка: Введите целое число."
+            exit 1
+    | "2" ->
+        printf "Введите строки:\n"
+        let mutable lines = []
+        let mutable line = Console.ReadLine()
+        if (String.IsNullOrEmpty(line)) then
+            printfn "\nСписок пуст."
+            exit 1
+        while not (String.IsNullOrEmpty(line)) do
+            lines <- line :: lines
+            line <- Console.ReadLine()
+        List.rev lines
+        
+    | _ ->
+        printfn "Ошибка: Некорректный выбор режима ввода."
+        exit 1
+
 
 printf "Введите символ для добавления в конец: "
 let symbol = Console.ReadLine()
+
 let check2 =
     if String.IsNullOrEmpty(symbol) then
         printfn "Ошибка: Вы не ввели символ."
@@ -27,20 +72,7 @@ let check2 =
     else
         symbol.[0]
 
-let generateRand (count: int) (minL: int) (maxL: int) =
-    let rnd = new Random()
-    [ for i in 1..count do
-        let stringL = rnd.Next(minL, maxL + 1)
-        let stringC = 
-            [ for j in 1..stringL do
-                let index = rnd.Next(26) 
-                char (int 'a' + index) ] 
-        String(List.toArray stringC) ]
 
-let addToList (add: char) (sList: string list) =
-    List.map (fun str -> str + string add) sList
-
-let MyList = generateRand check1 minL maxL
 printfn "Исходный список:"
 MyList |> List.iter (printfn "%s")
 
